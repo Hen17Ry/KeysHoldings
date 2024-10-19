ca2493a54af0cc8505a5e59ecde4b89ed625b1ed
Hen17Ry <106038140+Hen17Ry@users.noreply.github.com>
2024-10-19 01:27:47 +0100

    Co

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from app import create_app
 from dotenv import load_dotenv
+import os
 
+# Charger les variables d'environnement à partir de .env
+load_dotenv()
 
+# Créer l'application Flask
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run()
